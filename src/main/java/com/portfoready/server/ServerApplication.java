45a8a9332fab7cd40d3f358d95fa0495cefffb65
Earl Funk <earlfunk21@gmail.com>
2023-11-07 21:54:50 +0800

    First Commit

---
+++ src/main/java/com/portfoready/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.portfoready.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
